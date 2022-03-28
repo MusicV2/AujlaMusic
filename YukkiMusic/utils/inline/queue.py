7ea308bbb5c19d325154aea54ab0311dfd16c05c
MusicV2 <102456029+MusicV2@users.noreply.github.com>
2022-03-28 05:44:39 +0530

    Update queue.py

--- YukkiMusic/utils/inline/queue.py
+++ YukkiMusic/utils/inline/queue.py
@@ -38,7 +38,7 @@ def queue_markup(
                     text="❤️🅢🅣🅐🅣🅤🅢❤️", url=f"https://t.me/Punjabi_Status_Mania"
                 ),
                 InlineKeyboardButton(
-                    text="❤️𝙂𝙧𝙤𝙪𝙥❤️", url=f"https://t.me/Punjabi_ChatGroup"
+                    text="❤️𝔾ℝ𝕆𝕌ℙ❤️", url=f"https://t.me/Punjabi_ChatGroup"
                 ),
             ],
         [
